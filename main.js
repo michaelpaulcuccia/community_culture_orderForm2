166b93d7c699f6e93f9149349d24f6f29ae975ba
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2020-02-17 16:26:03 -0500

    dropdown issues

--- main.js
+++ main.js
@@ -10,8 +10,7 @@ function submitForm(event){
     var instagramhandle = getInputVal('instagramhandle');
     var phone = getInputVal('phone');
     var email = getInputVal('email');
-
-    //checkbox - comes back as UNDEFINED 
+    //checkbox - will show 'on', then true or false 
     var item1check = getInputVal('item1check');
 
     //dropdown - NOT WORKING
@@ -22,7 +21,7 @@ function submitForm(event){
     
     var ordernotes = getInputVal('ordernotes');
 
-    console.log(name, instagramhandle, phone, email, item1check, dropdownMenuButton, quantity1, ordernotes);
+    console.log(name, instagramhandle, phone, email, item1check, ordernotes);
 
 }
 
