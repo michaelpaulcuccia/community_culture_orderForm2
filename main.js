0214266df0517e840453fc0f1fe21fe5e6aab997
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2020-03-09 16:03:12 -0400

    order list needs work to show all instead of 1

--- main.js
+++ main.js
@@ -16,6 +16,47 @@ firebase.initializeApp(firebaseConfig);
 //reference collection - create name orderOne
 var orderOneRef = firebase.database().ref('orderOne');
 
+//****************************************************************************************************
+//getting data
+var ref = firebase.database().ref('orderOne');
+ref.on('value', gotData, errData);
+
+function gotData(data) {
+    //console.log(data);
+    console.log(data.val());
+    var order = data.val();
+    var keys = Object.keys(order);
+    console.log(keys);
+    for (var i = 0; i < keys.length; i++) {
+        var k = keys[i];
+        var name = order[k].name;
+        var instagramhandle = order[k].instagramhandle;
+        var phone = order[k].phone;
+        var email = order[k].email;
+        var item1check = order[k].item1check;
+        var size = order[k].size;
+        var quantity = order[k].quantity;
+        var ordernotes = order[k].ordernotes;
+        console.log(name, instagramhandle, phone, email, item1check, size, quantity, ordernotes);
+
+        document.querySelector('#name-return').innerHTML = name;
+        document.querySelector('#insta-return').innerHTML = instagramhandle;
+        document.querySelector('#phone-return').innerHTML = phone;
+        document.querySelector('#email-return').innerHTML = email;
+        document.querySelector('#item-return').innerHTML = item1check;
+        document.querySelector('#size-return').innerHTML = size;
+        document.querySelector('#quantity-return').innerHTML = quantity;
+        document.querySelector('#orderNotes-return').innerHTML = ordernotes;
+    }
+  }
+
+function errData(err){
+    console.log('ERROR!');
+    console.log(err);
+}
+
+//****************************************************************************************************
+
 //listen for form submit
 document.getElementById('form').addEventListener('submit', submitForm);
 
